eeaa23f923ea3dd6f86462ea41feac60c55aaf0b
Simon Nyholm Sørensen <simonnyholm@htomail>
2024-02-21 14:55:38 +0100

    add Home and Contact components

---
+++ src/pages/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Kontakt</h1>
+      <p>Kontakoplysninger</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Hjem</h1>
+      <p>her</p>
+    </div>
+  );
+};
+
+export default Home;
