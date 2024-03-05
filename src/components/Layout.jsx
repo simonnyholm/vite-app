34621642d0e87908a4a7b27d717993a2b74c9692
Simon Nyholm Sørensen <simonnyholm@htomail>
2024-03-05 14:51:16 +0100

    add page logo

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -14,6 +14,7 @@ export default function Layout() {
             Kontakt
           </NavLink>
         </nav>
+        <div>Simon Nyholm Sørensen</div>
       </header>
       <main>
         <Outlet />
